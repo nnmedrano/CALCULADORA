6838a13aa04c828b0630dc7a077e6ae15fe77920
nicole <nnmedrano94@hotmail.com>
2017-10-23 20:48:12 -0300

    calculadora

--- Program.cs
+++ Program.cs
@@ -4,9 +4,51 @@ namespace CALCULADORA
 {
     class Program
     {
-        static void Main(string[] args)
+        ///<sumary>
+        ///Muestra el menu
+        ///</sumary>
+
+        static void MostrarMenu()
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Que queres hacer?");
+            Console.WriteLine();
+            Console.WriteLine("1 - Sumar dos numeros");
+            Console.WriteLine("2 - Multiplicar dos numeros");
+            Console.WriteLine("0 - Salir");
+        }
+        
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine("Bienvenido a la calculadora");
+            MostrarMenu();
+
+            int opcion = int.Parse(Console.ReadLine());
+            switch(opcion)
+            {
+                case 1:
+                    //sumar TODO
+                    Console.WriteLine ("vamos a sumar");
+                    int resultado= sumar(4,3)
+                    Console.WriteLine("el resultado es" + resultado);
+                      
+                    break;
+                case 2:         
+                    //multiplicar TODO 
+                    Console.WriteLine ("vamos a multiplicar");
+                    int resultado= multiplicar(4,3)
+                    Console.WriteLine("el resultado es" + resultado);
+
+                case 3:
+                    //dividir 
+                              
+                    break;
+                default:
+                    Console.WriteLine("Opción inválida");                    
+                    MostrarMenu();
+                    break; 
+            }
+
         }
     }
 }
